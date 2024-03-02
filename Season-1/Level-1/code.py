9faca216318e06bc9d3c8f54b23bb534cfe84d46
art03javier <121649726+art03javier@users.noreply.github.com>
2024-03-02 13:55:04 +1100

    Fix vulnerability in code.py

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -17,10 +17,10 @@ from decimal import Decimal
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
-MAX_ITEM_AMOUNT = 100000 # maximum price of item in the shop
-MAX_QUANTITY = 100 # maximum quantity of an item in the shop
-MIN_QUANTITY = 0 # minimum quantity of an item in the shop
-MAX_TOTAL = 1e6 # maximum total amount accepted for an order
+MAX_ITEM_AMOUNT = 100000  # maximum price of item in the shop
+MAX_QUANTITY = 100  # maximum quantity of an item in the shop
+MIN_QUANTITY = 0  # minimum quantity of an item in the shop
+MAX_TOTAL = 1e6  # maximum total amount accepted for an order
 
 def validorder(order):
     payments = Decimal('0')
@@ -32,11 +32,15 @@ def validorder(order):
             if -MAX_ITEM_AMOUNT <= item.amount <= MAX_ITEM_AMOUNT:
                 payments += Decimal(str(item.amount))
         elif item.type == 'product':
-            if type(item.quantity) is int and MIN_QUANTITY < item.quantity <= MAX_QUANTITY and MIN_QUANTITY < item.amount <= MAX_ITEM_AMOUNT:
+            if (
+                type(item.quantity) is int
+                and MIN_QUANTITY < item.quantity <= MAX_QUANTITY
+                and MIN_QUANTITY < item.amount <= MAX_ITEM_AMOUNT
+            ):
                 expenses += Decimal(str(item.amount)) * item.quantity
         else:
             return "Invalid item type: %s" % item.type
-    
+
     if abs(payments) > MAX_TOTAL or expenses > MAX_TOTAL:
         return "Total amount payable for an order exceeded"
 
